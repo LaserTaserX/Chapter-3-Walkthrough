99799fa08f2b8f1d2f0a3b14ad4e42345b998d16
Alexander Frieders <afrieders@my.waketech.edu>
2026-02-11 14:09:02 -0500

    day 1

--- js03_txt.js
+++ js03_txt.js
@@ -4,9 +4,41 @@
 
      Tipton Turbines
      Program to display games results in a web table
-     Author: 
-     Date:   
+     Author: Alexander Frieders
+     Date: 2/11/2026
 
      Filename: js03.js
- */
+*/
 
+// Days of the week
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+window.addEventListener("load", addWeekdays);
+
+function addWeekdays() {
+    let i = 0;
+    let headingCells = document.getElementsByTagName("th");
+    while (i < 7) {
+        headingCells[i].innerHTML = weekDays[i];
+        i++;
+    }
+}
+
+window.addEventListener("load", showGames);
+
+function showGames() {
+    for (let i = 0; i < gameDates.length; i++) {
+        let gameInfo = "";
+        gameInfo += "<p>";
+        if (gameLocations[i] === "h") {
+            gameInfo += "vs. ";
+        } else if (gameLocations[i] === "a") {
+            gameInfo += "@ ";
+        }
+        gameInfo += gameOpponents[i] + "<br>";
+        gameInfo += gameResults[i] + ": (" + runsScored[i] + "-" + runsAllowed[i] + ")";
+        gameInfo += "</p>";
+        let tableCell = document.getElementById(gameDates[i]);
+        tableCell.insertAdjacentHTML("beforeEnd", gameInfo);
+    }
+}
